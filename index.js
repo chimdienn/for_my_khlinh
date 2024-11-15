4dfbaee54d93fdafcd84a4ba6468020cf7398d18
chimdienn <156086389+chimdienn@users.noreply.github.com>
2024-11-16 00:14:36 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+function nextSequence(){
+    level++;
+    if (level > maxLevel){
+        newGame();
+    }
+    else{
+        $("h1").html("level " + level + "/" + maxLevel);
+        var randomNumber = Math.floor(Math.random()*4);
+        var randomChosenColor = buttonColours[randomNumber];
+        gamePattern.push(randomChosenColor);
+
+        setTimeout(function(){
+            flashAnimation(randomChosenColor);
+            playSound(gamePatternSound[level]);
+        }, 200);
+        console.log(gamePattern);
+    }
+}
+
+function flashAnimation(id){
+    $("#"+id).animate({opacity: 0.4}, 70).animate({opacity: 1}, 70);
+}
+
+function playSound(id){
+    var sound = new Audio("sounds/" + id + ".mp3");
+    if (id == "wrong"){
+        sound.volume = 0.15;
+    }
+    sound.play();
+}
+
+function animatePress(id){
+    $("#"+id).addClass("pressed");
+    setTimeout(function(){
+        $("#"+id).removeClass("pressed")
+    }, 100);
+}
+
+function restartGame(){
+    $("h1").html("Game Over, Press This Line to Restart");
+    gamePattern = [];
+    userClickedPattern = [];
+    gameStarted = false;
+    levelWon = true;
+    level = 0;
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+        $("body").removeClass("game-over");
+    }, 200);
+}
+
+function newGame(){
+    $("h1").html("You Won! Press this line to restart");
+    var message = loveMessages[Math.floor(Math.random() * 4)];
+    $("footer").html(message);
+    gamePattern = [];
+    userClickedPattern = [];
+    gameStarted = false;
+    levelWon = true;
+    
+}
+
+const maxLevel = 13;
+const buttonColours = ["green", "red", "yellow", "blue"];
+const gamePatternSound = [0, "g3", "g3", "g4", "e4", "c4", "b4", "a4", "f4", "f4", "e4", "c4", "d4", "c4"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+var level = 0;
+var gameStarted = false;
+var levelWon = true;
+
+const loveMessages = [
+    "Em giỏi lắm! Anh iu em.",
+    "Hẹn ngày gặp nhau em nhé!",
+    "Khi nào gặp cho anh ôm cái nha ^^",
+    "anh nhớ khánh linh của anh."
+]
+
+
+$("h1").on("click", function(){
+    if (!gameStarted){
+        level = 0;
+        $("footer").html("Made with ❤️ by Trọng Đức.");
+        nextSequence();
+    }
+    gameStarted = true;
+});
+    
+$(".btn").on("click", function(){
+    if (level <= maxLevel){
+        var userChosenColor = $(this).attr("id");
+        userClickedPattern.push(userChosenColor);
+
+        animatePress(userChosenColor);
+        if (gamePattern.length === 0){
+            restartGame();
+        }
+
+        else {
+            for (var i=0; i<userClickedPattern.length; i++){
+                if (userClickedPattern[i] != gamePattern[i]){
+                    levelWon = false;
+                    break;
+                }
+                else if (i === userClickedPattern.length - 1){
+                    playSound(gamePatternSound[i+1]);
+                }
+            }
+            if (!levelWon){
+                restartGame();
+            }
+            else if (userClickedPattern.length === level && levelWon){
+                userClickedPattern = [];
+                setTimeout(function(){
+                    nextSequence();
+                }, 200); 
+            }
+        }
+    }
+});
